29601e6e89e400d7be8e5131de249d40c58aabc5
João Gabriel <111989051+JoaoGabrielGuedes@users.noreply.github.com>
2023-04-07 12:17:21 -0300

    Add files via upload
    Input idade, peso e nome;
    + Loop simples

--- ProjetoInicial/src/empresa/Principal.java
+++ ProjetoInicial/src/empresa/Principal.java
@@ -1,10 +1,42 @@
 package empresa;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		
+		int idade;
+		String nome;
+		double peso;
+		
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite idade, peso e nome: ");
+		idade = teclado.nextInt();
+		peso = teclado.nextDouble();
+		nome = teclado.next();
+		
+		System.out.println();
+		
+		System.out.println("Nome: " +nome);
+		System.out.printf("Idade: %d\n",idade);
+		System.out.printf("Peso: %.2f\n",peso);
+		
+		if (idade <18) {
+			System.out.println("Acesso bloqueado!");
+		    System.out.println("Acesso bloqueado!");
+		}
+		else if (idade <65) {
+			System.out.println("Adulto");
+		}
+		else {
+			System.out.println("Adulto idoso");
+		}
+		for(int i=0;i<10; i++) {
+			System.out.println("Valor: " +i);
+		}
+		
+				
 	}
 
 }
