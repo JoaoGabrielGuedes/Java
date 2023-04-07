a6ec41e33caef42e41aec19d8f2935f9a75e6fdc
João Gabriel <111989051+JoaoGabrielGuedes@users.noreply.github.com>
2023-04-07 12:59:43 -0300

    Add files via upload
    Palpites de valores com while, if e else

---
+++ Leonidas/src/empresa/Principal.java
@@ -0,0 +1,30 @@
+package empresa;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+		int palpite;
+		int valorCorreto=10000;
+		System.out.println("Digite seu palpite:");
+		palpite = teclado.nextInt();
+		
+		while(palpite != valorCorreto) {
+			if(palpite>valorCorreto) {
+				System.out.println("Um pouco menos...");
+			}
+		    else {
+			    System.out.println("Um pouco mais...");
+		    }
+		    System.out.println("Digite outro palpite: ");
+		    palpite = teclado.nextInt();
+		}
+		
+		System.out.println("Parabens, acertou!");
+
+	}
+
+}
